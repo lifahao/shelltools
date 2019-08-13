bf8252b8e49ea4e126c2fb88ce02fe10a337f168
lifahao <853232438@qq.com>
2019-08-13 21:19:05 +0800

    better installapp.sh

--- installapp.sh
+++ installapp.sh
@@ -1,1 +1,11 @@
-apt-get install vim git terminator 
+cp /etc/apt/sources.list /etc/apt/sources.list.back
+
+echo "
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+">>/etc/apt/sources.list
+apt-get update
+apt-get upgrade
+
+apt-get install vim git terminator zsh docker.io
+echo vim git terminator zsh docker have been downloaded!
+git clone https://github.com/
